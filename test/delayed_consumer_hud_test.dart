89ff275507d10376bde01dcd62e647c430f89494
Juan Rivera <juan@Juans-MBP.lan>
2020-10-10 12:13:48 -0400

    Initial cut of test cases

--- test/delayed_consumer_hud_test.dart
+++ test/delayed_consumer_hud_test.dart
@@ -1,13 +1,354 @@
-import 'package:flutter_test/flutter_test.dart';
-
 import 'package:delayed_consumer_hud/delayed_consumer_hud.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  testWidgets("DelayedHud no delay", (WidgetTester tester) async {
+    final key = Key('CircularProgressIndicator');
+    Widget hud = CircularProgressIndicator(key: key);
+
+    await tester.pumpWidget(MediaQuery(
+      data: MediaQueryData(size: Size(1024, 768)),
+      child: Directionality(
+        textDirection: TextDirection.ltr,
+        child: DelayedHud(
+          hud: hud,
+          child: Text('DelayedHud'),
+          delayedStart: null,
+          showHud: () => true,
+        ),
+      ),
+    ));
+
+    expect(find.byKey(key), findsOneWidget);
   });
+
+  testWidgets("DelayedHud no delay and generate widget callback",
+      (WidgetTester tester) async {
+    final key = Key('CircularProgressIndicator');
+    await tester.pumpWidget(MediaQuery(
+      data: MediaQueryData(size: Size(1024, 768)),
+      child: Directionality(
+        textDirection: TextDirection.ltr,
+        child: DelayedHud(
+          hudWidget: () {
+            return CircularProgressIndicator(key: key);
+          },
+          child: Text('DelayedHud'),
+          delayedStart: null,
+          showHud: () => true,
+        ),
+      ),
+    ));
+
+    expect(find.byKey(key), findsOneWidget);
+  });
+
+  testWidgets("DelayedHud with delay", (WidgetTester tester) async {
+    final key = Key('CircularProgressIndicator');
+    Widget hud = CircularProgressIndicator(key: key);
+
+    final delay = Duration(seconds: 1);
+
+    await tester.pumpWidget(MediaQuery(
+      data: MediaQueryData(size: Size(1024, 768)),
+      child: Directionality(
+        textDirection: TextDirection.ltr,
+        child: DelayedHud(
+          hud: hud,
+          child: Text('DelayedHud'),
+          delayedStart: delay,
+          showHud: () => true,
+        ),
+      ),
+    ));
+
+    expect(find.byKey(key), findsNothing);
+
+    await tester.pump(delay);
+
+    expect(find.byKey(key), findsOneWidget);
+  });
+
+  testWidgets("DelayedHud1 with provider and delay",
+      (WidgetTester tester) async {
+    final key = Key('CircularProgressIndicator');
+    Widget hud = CircularProgressIndicator(key: key);
+
+    final delay = Duration(seconds: 1);
+
+    var testViewModel = TestViewModel();
+
+    await tester.pumpWidget(MediaQuery(
+      data: MediaQueryData(size: Size(1024, 768)),
+      child: ChangeNotifierProvider<TestViewModel>.value(
+          value: testViewModel,
+          builder: (context, child) {
+            return Directionality(
+              textDirection: TextDirection.ltr,
+              child: DelayedHud1<TestViewModel>(
+                hud: hud,
+                child: Text('DelayedHud1<TestViewModel>'),
+                delayedStart: delay,
+                showHud: (viewModel) {
+                  return viewModel.isBusy;
+                },
+              ),
+            );
+          }),
+    ));
+
+    expect(find.byKey(key), findsNothing);
+
+    await tester.pump(delay);
+
+    testViewModel.isBusy = true;
+
+    await tester.pump();
+
+    expect(find.byKey(key), findsNothing);
+
+    await tester.pump(delay);
+
+    expect(find.byKey(key), findsOneWidget);
+
+    testViewModel.isBusy = false;
+
+    await tester.pump();
+
+    expect(find.byKey(key), findsNothing);
+  });
+
+  testWidgets("DelayedHud1 with provider and delay and cancel",
+      (WidgetTester tester) async {
+    final key = Key('CircularProgressIndicator');
+    Widget hud = CircularProgressIndicator(key: key);
+
+    final delay = Duration(seconds: 2);
+    final halfDelay = Duration(seconds: 1);
+
+    var testViewModel = TestViewModel();
+
+    await tester.pumpWidget(MediaQuery(
+      data: MediaQueryData(size: Size(1024, 768)),
+      child: ChangeNotifierProvider<TestViewModel>.value(
+          value: testViewModel,
+          builder: (context, child) {
+            return Directionality(
+              textDirection: TextDirection.ltr,
+              child: DelayedHud1<TestViewModel>(
+                hud: hud,
+                child: Text('DelayedHud1<TestViewModel>'),
+                delayedStart: delay,
+                showHud: (viewModel) {
+                  return viewModel.isBusy;
+                },
+              ),
+            );
+          }),
+    ));
+
+    expect(find.byKey(key), findsNothing);
+
+    await tester.pump(delay);
+
+    testViewModel.isBusy = true;
+
+    await tester.pump();
+
+    expect(find.byKey(key), findsNothing);
+
+    await tester.pump(halfDelay);
+
+    expect(find.byKey(key), findsNothing);
+
+    testViewModel.isBusy = false;
+
+    await tester.pump(halfDelay);
+
+    expect(find.byKey(key), findsNothing);
+  });
+
+  testWidgets("DelayedHud2 with provider and delay",
+      (WidgetTester tester) async {
+    final key = Key('CircularProgressIndicator');
+    Widget hud = CircularProgressIndicator(key: key);
+
+    final delay = Duration(seconds: 1);
+
+    var testViewModel1 = TestViewModel();
+    var testViewModel2 = TestViewModel();
+
+    await tester.pumpWidget(
+      MediaQuery(
+        data: MediaQueryData(size: Size(1024, 768)),
+        child: MultiProvider(
+            providers: [
+              ChangeNotifierProvider<TestViewModel>.value(
+                  value: testViewModel1),
+              ChangeNotifierProvider<TestViewModel>.value(
+                  value: testViewModel2),
+            ],
+            builder: (context, child) {
+              return Directionality(
+                textDirection: TextDirection.ltr,
+                child: DelayedHud2<TestViewModel, TestViewModel>(
+                  hud: hud,
+                  child: Text('DelayedHud2<TestViewModel, TestViewModel>'),
+                  delayedStart: delay,
+                  showHud: (viewModel1, viewModel2) {
+                    return viewModel1.isBusy || viewModel2.isBusy;
+                  },
+                ),
+              );
+            }),
+      ),
+    );
+
+    expect(find.byKey(key), findsNothing);
+
+    await tester.pump(delay);
+
+    testViewModel1.isBusy = true;
+
+    await tester.pump();
+
+    expect(find.byKey(key), findsNothing);
+
+    await tester.pump(delay);
+
+    expect(find.byKey(key), findsOneWidget);
+
+    testViewModel1.isBusy = false;
+
+    await tester.pump();
+
+    expect(find.byKey(key), findsNothing);
+
+    testViewModel2.isBusy = true;
+
+    await tester.pump();
+
+    expect(find.byKey(key), findsNothing);
+
+    await tester.pump(delay);
+
+    expect(find.byKey(key), findsOneWidget);
+
+    testViewModel2.isBusy = false;
+
+    await tester.pump();
+
+    expect(find.byKey(key), findsNothing);
+  });
+
+  testWidgets("DelayedHud3 with provider and delay",
+      (WidgetTester tester) async {
+    final key = Key('CircularProgressIndicator');
+    Widget hud = CircularProgressIndicator(key: key);
+
+    final delay = Duration(seconds: 1);
+
+    var testViewModel1 = TestViewModel();
+    var testViewModel2 = TestViewModel();
+    var testViewModel3 = TestViewModel();
+
+    await tester.pumpWidget(
+      MediaQuery(
+        data: MediaQueryData(size: Size(1024, 768)),
+        child: MultiProvider(
+            providers: [
+              ChangeNotifierProvider<TestViewModel>.value(
+                  value: testViewModel1),
+              ChangeNotifierProvider<TestViewModel>.value(
+                  value: testViewModel2),
+              ChangeNotifierProvider<TestViewModel>.value(
+                  value: testViewModel3),
+            ],
+            builder: (context, child) {
+              return Directionality(
+                textDirection: TextDirection.ltr,
+                child: DelayedHud2<TestViewModel, TestViewModel>(
+                  hud: hud,
+                  child: Text('DelayedHud2<TestViewModel, TestViewModel>'),
+                  delayedStart: delay,
+                  showHud: (viewModel1, viewModel2) {
+                    return viewModel1.isBusy || viewModel2.isBusy;
+                  },
+                ),
+              );
+            }),
+      ),
+    );
+
+    expect(find.byKey(key), findsNothing);
+
+    await tester.pump(delay);
+
+    testViewModel1.isBusy = true;
+
+    await tester.pump();
+
+    expect(find.byKey(key), findsNothing);
+
+    await tester.pump(delay);
+
+    expect(find.byKey(key), findsOneWidget);
+
+    testViewModel1.isBusy = false;
+
+    await tester.pump();
+
+    expect(find.byKey(key), findsNothing);
+
+    testViewModel2.isBusy = true;
+
+    await tester.pump();
+
+    expect(find.byKey(key), findsNothing);
+
+    await tester.pump(delay);
+
+    expect(find.byKey(key), findsOneWidget);
+
+    testViewModel2.isBusy = false;
+
+    await tester.pump();
+
+    expect(find.byKey(key), findsNothing);
+
+    testViewModel3.isBusy = true;
+
+    await tester.pump();
+
+    expect(find.byKey(key), findsNothing);
+
+    await tester.pump(delay);
+
+    expect(find.byKey(key), findsOneWidget);
+
+    testViewModel3.isBusy = false;
+
+    await tester.pump();
+
+    expect(find.byKey(key), findsNothing);
+  });
+}
+
+class TestViewModel extends ChangeNotifier {
+  bool _isBusy = false;
+
+  set isBusy(bool value) {
+    _isBusy = value;
+    notifyListeners();
+  }
+
+  bool get isBusy {
+    return _isBusy;
+  }
+
+  bool get isNotBusy {
+    return !_isBusy;
+  }
 }
